2793f9b671e52a41a3d1ba20dcb9608e469ae6c6
Morten Lyngberg Pedersen <mort73h9@edu.easj.dk>
2020-11-12 22:51:47 +0100

    Added test program

---
+++ ZeymerZoneTestConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZeymerZoneTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
