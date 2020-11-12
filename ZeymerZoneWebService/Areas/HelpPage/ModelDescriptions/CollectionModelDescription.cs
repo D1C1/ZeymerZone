490e8b745ab26642a12582e411c52b43a8ab1b44
Morten Lyngberg Pedersen <mort73h9@edu.easj.dk>
2020-11-12 12:54:04 +0100

    Revert "Oprettet Web service projekt"
    This reverts commit 701ab23c670259e6de62ac488ffa39b96331bdab.

--- ZeymerZoneWebService/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace ZeymerZoneWebService.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- ZeymerZoneWebService/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ZeymerZoneWebService.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
