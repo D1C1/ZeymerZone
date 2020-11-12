5fead0ff8dfa009810a3f02e9c8de81d2132e57f
Morten Lyngberg Pedersen <mort73h9@edu.easj.dk>
2020-11-12 13:52:15 +0100

    Lavet context + første controller

---
+++ ZeymerZoneWebService/Kalender.cs
@@ -0,0 +1,21 @@
+namespace ZeymerZoneWebService
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Kalender")]
+    public partial class Kalender
+    {
+        [Key]
+        public int Kalender_Id { get; set; }
+
+        public int Vejleder_Id { get; set; }
+
+        public DateTime CalenderDate { get; set; }
+
+        public virtual Vejleder Vejleder { get; set; }
+    }
+}
---
+++ ZeymerZoneWebService/Konsultation.cs
@@ -0,0 +1,26 @@
+namespace ZeymerZoneWebService
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Konsultation")]
+    public partial class Konsultation
+    {
+        [Key]
+        public int Konsultation_Id { get; set; }
+
+        public int Vejleder_Id { get; set; }
+
+        public int Kunde_Id { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime Konsultation_date { get; set; }
+
+        public virtual Kunde Kunde { get; set; }
+
+        public virtual Vejleder Vejleder { get; set; }
+    }
+}
