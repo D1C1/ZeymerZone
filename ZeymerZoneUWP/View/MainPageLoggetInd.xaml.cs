45adf5b63edd1abe76785ec1ce9e7632bddd443e
Morten Lyngberg Pedersen <mort73h9@edu.easj.dk>
2020-11-16 10:51:59 +0100

    Lavet taskbar til logget ind samt flyttet til mappe

--- ZeymerZoneUWP/View/MainPageLoggetInd.xaml.cs
+++ ZeymerZoneUWP/View/MainPageLoggetInd.xaml.cs
@@ -13,25 +13,33 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace ZeymerZoneUWP
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPageLoggetInd : Page
     {
-        public MainPageViewModel MPViewModel { get; set; }
-        public MainPage()
+        public MainPageLoggetInd()
         {
-            //MPViewModel = new MainPageViewModel();
             this.InitializeComponent();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            this.Frame.Navigate(typeof(LoginPage));
+            this.Frame.Navigate(typeof(Profilside));
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Opretbruger));
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(BrugerLogs));
         }
     }
 }
