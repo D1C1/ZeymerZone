b2c0def71fcfa739d4f4936be74569a668022ba9
Morten Lyngberg Pedersen <mort73h9@edu.easj.dk>
2020-12-02 12:32:11 +0100

    Lavet ny viewmodel

--- ZeymerZoneUWP/ViewModel/KonsultationViewModel.cs
+++ ZeymerZoneUWP/ViewModel/KonsultationViewModel.cs
@@ -9,4 +9,32 @@ namespace ZeymerZoneUWP.ViewModel
     class KonsultationViewModel
     {
     }
+
+    #region Henter kunde
+    public Kunde CurrentKunde
+    {
+        get
+        {
+            return _currentKunde;
+        }
+        set
+        {
+            _currentKunde = value;
+            NotifyPropertyChanged();
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+
+    private async void SetCurrent()
+    {
+        CurrentKunde = await PersistencyService<Kunde>.HentDataDisk("KundeCurrent");
+    }
+
+    #endregion
 }
