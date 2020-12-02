2ce7ec50ef5b0d773a7225c8acdd0db079138c41
Morten Lyngberg Pedersen <mort73h9@edu.easj.dk>
2020-12-02 12:35:23 +0100

    Update KonsultationViewModel.cs

--- ZeymerZoneUWP/ViewModel/KonsultationViewModel.cs
+++ ZeymerZoneUWP/ViewModel/KonsultationViewModel.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ZeymerZoneUWP.ViewModel
 {
-    class KonsultationViewModel
+    public class KonsultationViewModel
     {
+        private Kunde _currentKunde;
+
+        public KonsultationViewModel()
+        {
+            SetCurrent();
+        }
 
         #region Henter kunde
         public Kunde CurrentKunde
