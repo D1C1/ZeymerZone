3b461d75afe7cb6a9e4271f18b64449ed58b1eeb
Jeppe Hauman <jeppe_hauman@hotmail.com>
2020-12-10 09:40:35 +0100

    Create VejlederViewModel.cs
    Start på vejlederviewmodel

---
+++ ZeymerZoneUWP/ViewModel/VejlederViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeymerZoneUWP.ViewModel
+{
+    public class VejlederViewModel : INotifyPropertyChanged
+    {
+        public VejlederViewModel()
+        {
+            CurrentVejleder = new Vejleder(); 
+            
+        }
+
+        private Vejleder _currentVejleder;
+
+        public Vejleder CurrentVejleder
+        {
+            get { return _currentVejleder; }
+            set { _currentVejleder = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<Vejleder> OC_Vejledere { get; set; } = new ObservableCollection<Vejleder>();
+
+        public ICollection<Vejleder> Vejledere { get; set; }
+
+        public async void SetVejleder()
+        {
+            Vejledere = await PersistencyService<ICollection<Vejleder>>.HentData("vejleders");
+            foreach (var item in Vejledere)
+            {
+                OC_Vejledere.Add(item);
+            }
+        }
+
+
+
+
+        #region PropertyChanged Implementering
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
